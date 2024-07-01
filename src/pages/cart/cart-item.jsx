5a72b1fe83c3192b9c3b7794646f47d2f62f27af
M-Ajay-Yadav <majay805@gmail.com>
2024-07-01 16:02:53 +0530

    need to work on it

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,24 +1,30 @@
-import React,{ useContext } from 'react'
-import { ShopContext } from '../../context/shop-context';
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
 export const CartItem = (props) => {
-    const { id , productName, price, productImage} = props.data;
-    const { cartItems, addToCart , removeFromCart,updateCartItemCount } = useContext(ShopContext);
+  const { id, productName, price, productImage } = props.data;
+  const { cartItems, addToCart, removeFromCart, updateCartItemCount } =
+    useContext(ShopContext);
+
+  const itemCount = cartItems[id] || 0;
 
   return (
-    <div className='cartItem'>
-        <img src={productImage} alt="" />
-        <div className='desccription'>
-            <p>
-                <b>{productName}</b>
-            </p>
-            <p> ${price}</p>
-            <div className='countHandler'>
-                <button onClick={() => removeFromCart(id)}>-</button>
-                <input value= {cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value),id)} />
-                <button onClick={() => addToCart(id)}>+</button>
-            </div>
+    <div className="cartItem">
+      <img src={productImage} alt="" />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p> ${price}</p>
+        <div className="countHandler">
+          <button onClick={() => removeFromCart(id)}>-</button>
+          <input
+            value={itemCount}
+            onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
+          />
+          <button onClick={() => addToCart(id)}>+</button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { PRODUCTS } from '../../products';
-import { Product } from './product';
+import React from "react";
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
 import "./shop.css";
 
-export const Shop = ({id}) => {
+export const Shop = ({ id }) => {
   // const id = {id}
   return (
-    <div className='shop'>
-        <div className='shopTitle'>
-            <h1> Tech Shop </h1>
-        </div>
-        <div className='products '>
-          {PRODUCTS.map((product) => (
-            <Product data={product} id={id} />
-          ) )}
-        </div>
+    <div className="shop">
+      <div className="shopTitle">
+        <h1> Tech Shop </h1>
+      </div>
+      <div className="products ">
+        {PRODUCTS.map((product) => (
+          <Product data={product} id={id} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
